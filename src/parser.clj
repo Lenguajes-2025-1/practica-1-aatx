e5c0df3078fcd3508dc8deaf0b44afadfe87d123
ADRIANXOSPA130 <adrianxolalpa11@gmail.com>
2024-10-17 01:58:39 -0600

    parser.clj

--- src/parser.clj
+++ src/parser.clj
@@ -2,11 +2,23 @@
   (:require [grammars :as wae]))
 
 (defn parser-AE
-    "Por implementar"
-    [exp]
-    ())
+    (cond 
+      (number? exp) (numG exp)
+      (symbol? exp) (idG exp)
+      (sequential? exp)
+      (case (first exp)
+        + (addG (parser-AE (second exp)) (parser-AE (nth exp 2)))
+        - (subG (parser-AE (second exp)) (parser-AE (nth exp 2))))
+      :else (throw (Exception. (str "Expresión no valida: " exp)))))
 
-(defn parser-WAE
-    "Por implemntar"
-    [exp]
-    ())
+(defn parser-WAE [exp]
+  (cond
+      (number? exp) (numG exp)
+      (symbol? exp) (idG exp)
+      (sequential? exp)
+      (case (first exp)
+        + (addG (parser-WAE (second exp)) (parser-WAE (nth exp 2)))
+        - (subG (parser-WAE (second exp)) (parser-WAE (nth exp 2)))
+        with (let [[_ id value body] exp]
+               (WithG (parser-WAE id) (parser-WAE value) (parser-WAE body))))
+      :else (throw (Exception. (str "Expresión no valida: " exp)))))
