b4bf8b8b5cef5c840f657c8f882a75e427857895
ADRIANXOSPA130 <adrianxolalpa11@gmail.com>
2024-10-17 02:02:09 -0600

    parser.clj

--- src/parser.clj
+++ src/parser.clj
@@ -1,24 +1,23 @@
 (ns parser
   (:require [grammars :as wae]))
 
-(defn parser-AE
+(defn parser-AE [exp]
     (cond 
-      (number? exp) (numG exp)
-      (symbol? exp) (idG exp)
+      (number? exp) (wae/numG exp)
       (sequential? exp)
       (case (first exp)
-        + (addG (parser-AE (second exp)) (parser-AE (nth exp 2)))
-        - (subG (parser-AE (second exp)) (parser-AE (nth exp 2))))
+        + (wae/addG (parser-AE (second exp)) (parser-AE (nth exp 2)))
+        - (wae/subG (parser-AE (second exp)) (parser-AE (nth exp 2))))
       :else (throw (Exception. (str "Expresión no valida: " exp)))))
 
 (defn parser-WAE [exp]
   (cond
-      (number? exp) (numG exp)
-      (symbol? exp) (idG exp)
+      (number? exp) (wae/numG exp)
+      (symbol? exp) (wae/idG exp)
       (sequential? exp)
       (case (first exp)
-        + (addG (parser-WAE (second exp)) (parser-WAE (nth exp 2)))
-        - (subG (parser-WAE (second exp)) (parser-WAE (nth exp 2)))
+        + (wae/addG (parser-WAE (second exp)) (parser-WAE (nth exp 2)))
+        - (wae/subG (parser-WAE (second exp)) (parser-WAE (nth exp 2)))
         with (let [[_ id value body] exp]
-               (WithG (parser-WAE id) (parser-WAE value) (parser-WAE body))))
+               (wae/withG (parser-WAE id) (parser-WAE value) (parser-WAE body))))
       :else (throw (Exception. (str "Expresión no valida: " exp)))))
